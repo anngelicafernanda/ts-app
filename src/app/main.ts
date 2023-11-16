d55663dd5165d1261d478d230870d7ec8cec8f3b
s7047759 <devanngelc@gmail.com>
2023-11-16 12:17:53 -0300

    Ejemplo de CRUD

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,28 @@
-import { addProduct } from './products/product.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-  id: '1',
-  title: 'p1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 90,
-  category: {
-    id: '1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    name: 'prueba',
-  },
-});
+import { addProduct, products } from './products/product.service';
+
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    id: faker.string.uuid(),
+    title: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    color: faker.color.human(),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElement([]),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.number.int({ min: 0, max: 100 }),
+    category: {
+      id: faker.string.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+  });
+}
+
+console.log(products);
