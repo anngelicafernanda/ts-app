c2b4d622b2a685cf1efcfb95f6053aa696514598
Angelica Contreras <afernandac94@gmail.com>
2023-11-27 14:31:17 -0300

    Fix tags type

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,17 +5,17 @@ import { addProduct, products } from './products/product.service';
 for (let index = 0; index < 50; index++) {
   addProduct({
     id: faker.string.uuid(),
-    title: faker.commerce.productName(),
     description: faker.commerce.productDescription(),
     image: faker.image.url(),
     color: faker.color.human(),
-    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
     price: parseInt(faker.commerce.price(), 10),
     isNew: faker.datatype.boolean(),
-    tags: faker.helpers.arrayElement([]),
+    tags: faker.string.alpha(),
+    title: faker.commerce.productName(),
     createdAt: faker.date.recent(),
     updatedAt: faker.date.recent(),
     stock: faker.number.int({ min: 0, max: 100 }),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
     category: {
       id: faker.string.uuid(),
       name: faker.commerce.department(),
